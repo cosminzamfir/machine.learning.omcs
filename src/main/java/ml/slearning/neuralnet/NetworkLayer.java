ebd2c9f500df17a64e3c3a9e4578790306cfb042
Cosmin Zamfir <cosminzamfir@yahoo.com>
2016-01-15 16:47:40 +0100

    Neural networks

--- src/main/java/ml/slearning/neuralnet/NetworkLayer.java
+++ src/main/java/ml/slearning/neuralnet/NetworkLayer.java
@@ -12,28 +12,37 @@ public class NetworkLayer {
 
 	/** Not null if the first hidden layer */
 	private DataSet inputDataSet;
-	
+
 	/** Null if the first hidden layer */
 	private NetworkLayer inputLayer;
-	
+
 	/** Null if this is the output layer */
 	private NetworkLayer outputLayer;
-	
+
 	private List<Sigmoid> units;
-	
+
 	/**
 	 * The weight of the input of each unit in the previous layer to each unit in this layer 
 	 */
 	private double[][] inputCoefficients;
-	
+
 	/**
 	 * The weight of the output if each unit in this layer to each unit in the next layer
 	 */
 	private double[][] outputCoefficients;
 
-
-	public NetworkLayer() {
+	public NetworkLayer(DataSet inputDataSet, List<Sigmoid> units) {
 		super();
+		this.inputDataSet = inputDataSet;
+		this.units = units;
+		this.inputCoefficients = new double[inputDataSet.getAttributes().size()][units.size()];
+	}
+	
+	public NetworkLayer(NetworkLayer inputLayer, List<Sigmoid> units) {
+		super();
+		this.inputLayer = inputLayer;
+		this.units = units;
+		this.inputCoefficients = new double[inputLayer.getUnits().size()][units.size()];
 	}
 
 	public NetworkLayer getInputLayer() {
@@ -59,14 +68,20 @@ public class NetworkLayer {
 	public void setUnits(List<Sigmoid> units) {
 		this.units = units;
 	}
-	
+
 	public double[][] getInputCoefficients() {
 		return inputCoefficients;
 	}
-	
+
 	public double[][] getOutputCoefficients() {
 		return outputCoefficients;
 	}
-	
-	
+
+	public DataSet getInputDataSet() {
+		return inputDataSet;
+	}
+
+	public void setInputDataSet(DataSet inputDataSet) {
+		this.inputDataSet = inputDataSet;
+	}
 }
