eb3330f0331a93a25f5f5231c8e0801df112e766
Cosmin Zamfir <cosminzamfir@yahoo.com>
2016-06-09 17:29:25 +0200

    MDP builder

--- src/main/java/ml/rl/mdp/model/MDPBuilder.java
+++ src/main/java/ml/rl/mdp/model/MDPBuilder.java
@@ -10,44 +10,46 @@ import java.util.Set;
 import org.apache.log4j.Logger;
 
 /**
- * Build an {@link MDP} given an {@link FullTransitionInfoEnvironment} and an initial {@link State}
+ * Build an {@link MDP} given an {@link FullTransitionInfoEnvironment}
  * @author eh2zamf
  *
  */
 public class MDPBuilder {
 
 	private static final Logger log = Logger.getLogger(MDPBuilder.class);
-	private State initialState;
 	private FullTransitionInfoEnvironment environment;
 	private MDP mdp;
-	/** Map State to whether it has been processed. Reason is to remember visited States to avoid re-processing them */
+	/** Map State to whether it has been processed. Reason is to keep all States to be processed and remember visited States to avoid re-processing them */
 	private Map<State, Boolean> cache = new HashMap<State, Boolean>();
 
 	public MDPBuilder(FullTransitionInfoEnvironment environment) {
 		super();
-		this.initialState = environment.initialState();
 		this.environment = environment;
 		mdp = MDP.instance();
 	}
 
 	public MDP build() {
-		State state = initialState;
+		State state = environment.initialState();
 		cache.put(state, false);
 		while ((state = pollNextState()) != null) {
 
 			if (!environment.isTerminal(state)) {
 				processState(state);
 				cache.put(state, true);
-				log.debug("Processed non-terminal state and set it as processed: " + state + ". Cache size: " + cache.size());
+				log.debug("Processed non-terminal state and mark it as processed: " + state + ". Cache size: " + cache.size());
 			} else {
 				cache.put(state, true);
-				log.debug("Set terminal state as processed: " + ". Cache size: " + cache.size());
+				log.debug("Mark terminal state as processed: " + ". Cache size: " + cache.size());
 			}
 		}
 		log.info("Done. MDP number of states: " + mdp.getStates().size());
 		return mdp;
 	}
 
+	/**
+	 * Retrieve next unprocessed State from the {@link #cache}
+	 * @return
+	 */
 	private State pollNextState() {
 		for (Iterator<State> iterator = cache.keySet().iterator(); iterator.hasNext();) {
 			State state = iterator.next();
