fd3cd4766a0f4878edb91cd0f9b8c25f1675e2d9
Cosmin Zamfir <cosminzamfir@yahoo.com>
2016-07-01 14:57:10 +0200

    continued

--- src/main/java/ml/rl/hw6/BarBrawl.java
+++ src/main/java/ml/rl/hw6/BarBrawl.java
@@ -1,6 +1,9 @@
 package ml.rl.hw6;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.lang.model.type.IntersectionType;
 
 import util.MLUtils;
 
@@ -33,39 +36,151 @@ public class BarBrawl {
 	// 	- the instigator cannot be outside S
 	//  - the peacemaker cannot be in S
 	//if there is no fight =>
-	//  - no conclusion
+	//  - it is not possible that the 
 
-	private Static 
+	private static String FIGTH = "FIGHT";
+	private static String NO_FIGHT = "NO FIGHT";
+	private static String DONTKNOW = "I DON'T KNOW";
 	private int n;
 	private int[][] subsets;
-	private int[][] fightOccured;
-	private int[] peacemakerPositions;
-	private int[] instigatorPositions;
+	private int[] fightOccured;
+	private List<List<Combination>> possiblePositions; 
 
-	public BarBrawl(int n, int[][] subsets, int[][] fightOccured) {
+	public BarBrawl(int n, int[][] subsets, int[] fightOccured) {
 		super();
 		this.n = n;
 		this.subsets = subsets;
 		this.fightOccured = fightOccured;
+		initialize();
 	}
 
 	public void initialize() {
-		peacemakerPositions = new int[n];
-		instigatorPositions = new int[n];
-		for (int i = 0; i < peacemakerPositions.length; i++) {
-			peacemakerPositions[i] = 1;
-			instigatorPositions[i] = 1;
+		possiblePositions = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			List<Combination> l = new ArrayList<>();
+			l.add(Combination._00);
+			l.add(Combination._01);
+			l.add(Combination._10);
+			l.add(Combination._11);
+			possiblePositions.add(l);
 		}
-
+		
+	}
+	
+	public void run() {
+		for (int i = 0; i < subsets.length; i++) {
+			int[] subset = subsets[i];
+			boolean fight = fightOccured[i] == 1;
+			String res = evaluate(subset, fight);
+			System.out.print(res + ",");
+			if(res.equals(DONTKNOW)) {
+				learn(subset, fight);
+			}
+		}
+	}
+	
+	
+	//must keep 2 lists: possible positions of the pacemaker and possible positions of the pacemaker
+	//if there is a fight =>
+	// 	- the instigator cannot be outside S
+	//  - the peacemaker cannot be in S
+	//if there is no fight =>
+	//  - it cannot be that 
+	public void learn(int[] subset, boolean fight) {
+		if(fight) {
+			for (int i = 0; i < subset.length; i++) {
+				//the instigator cannot be outside S
+				if(subset[i] == 0) {
+					possiblePositions.remove(Combination._10);
+					possiblePositions.remove(Combination._11);
+				}
+				//the peacemaker cannot be in S
+				if(subset[i] == 1) {
+					peacemakerPositions[i] = 0;
+				}
+			}
+		}
 	}
 
-	public String evaluate(boolean[] subset, boolean fightOccured) {
-		if (!intersect(subset, instigatorPositions)) {
-			retrun
+	public String evaluate(int[] subset, boolean fight) {
+		Position instigatorPos = getPosition(subset, instigatorPositions);
+		Position peacemakerPos = getPosition(subset, peacemakerPositions);
+		if(instigatorPos == Position.OUT || peacemakerPos == Position.IN) {
+			return NO_FIGHT;
 		}
+		if(instigatorPos == Position.IN && peacemakerPos == Position.OUT) {
+			return FIGTH;
+		}
+		return DONTKNOW;
+	}
+	
+	
+
+	private Position getPosition(int[] subset, int[] possiblePositions) {
+		boolean canBeIn = intersect(subset, possiblePositions);
+		boolean canBeOut = intersect(complement(subset), possiblePositions);
+		if(canBeIn && canBeOut) {
+			return Position.BOTH;
+		}
+		if(canBeIn) {
+			return Position.IN;
+		}
+		return Position.OUT;
 	}
 
-	public void learn(boolean[] subset, boolean fightOccured) {
+
+
+	private int[] complement(int[] subset) {
+		int[] res = new int[n];
+		for (int i = 0; i < n; i++) {
+			res[i] = 1 - subset[i];
+		}
+		return res;
+	}
+
+	private boolean intersect(int[] subset, int[] possiblePositions) {
+		for (int i = 0; i < n; i++) {
+			if(subset[i] == 1 && possiblePositions[i] == 1) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+
+	enum Position {
+		IN, OUT, BOTH
+	};
+	
+	enum Combination {
+		_00, _01, _10, _11;
+		
+		public boolean isInstigator() {
+			return this == _10 || this == _11; 
+		}
+		
+		public boolean isPeacemaker() {
+			return this == _01 || this == _11;
+		}
+		
+		public boolean isInstigator(Combination[] cs) {
+			for (Combination combination : cs) {
+				if(combination.isInstigator()) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public boolean isPeaceMaker(Combination[] cs) {
+			for (Combination combination : cs) {
+				if(combination.isPeacemaker()) {
+					return true;
+				}
+			}
+			return false;
+		}
 
 	}
 }
--- src/main/java/ml/rl/hw6/BarBrawlTest.java
+++ src/main/java/ml/rl/hw6/BarBrawlTest.java
@@ -9,7 +9,12 @@ public class BarBrawlTest {
 	@Test
 	public void test1() throws Exception {
 		int numPatrons = 4;
-		int[][] atEstablishment = {{0,1,1,1},{0,0,0,1},{1,1,1,1},{0,0,1,1},{0,1,1,1},{0,0,0,1},{1,1,1,1},{0,1,1,1},{1,1,1,1},{0,1,1,1},{0,0,0,1},{0,0,1,1},{0,0,0,1},{1,1,1,1},{1,1,1,1},{1,1,1,1},{1,1,1,1},{0,1,1,1},{0,0,1,1},{1,1,1,1},{1,1,1,1},{0,1,1,1},{1,1,1,1},{1,1,1,1},{0,0,0,1},{0,1,1,1},{1,1,1,1},{0,1,1,1},{0,0,1,1},{0,1,1,1},{1,1,1,1},{1,1,1,1}}
-		int[] fightOccurred = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+		int[][] atEstablishment = { { 0, 1, 1, 1 }, { 0, 0, 0, 1 }, { 1, 1, 1, 1 }, { 0, 0, 1, 1 }, { 0, 1, 1, 1 }, { 0, 0, 0, 1 }, { 1, 1, 1, 1 },
+				{ 0, 1, 1, 1 }, { 1, 1, 1, 1 }, { 0, 1, 1, 1 }, { 0, 0, 0, 1 }, { 0, 0, 1, 1 }, { 0, 0, 0, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 },
+				{ 1, 1, 1, 1 }, { 0, 1, 1, 1 }, { 0, 0, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 0, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 0, 0, 0, 1 },
+				{ 0, 1, 1, 1 }, { 1, 1, 1, 1 }, { 0, 1, 1, 1 }, { 0, 0, 1, 1 }, { 0, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 } };
+		int[] fightOccurred = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		String res = "I DON'T KNOW,I DON'T KNOW,NO FIGHT,I DON'T KNOW,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT,NO FIGHT";
+		new BarBrawl(numPatrons, atEstablishment, fightOccurred).run();
 	}
 }
